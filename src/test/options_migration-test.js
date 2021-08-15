ad33f5e72bad5a09cfe3becdf47622d623fca38d
Rob Lemley <rob@calypsoblue.org>
2021-08-14 23:18:59 -0400

    test fixups

--- src/test/options_migration-test.js
+++ src/test/options_migration-test.js
@@ -23,68 +23,68 @@ const DEFAULTS = {
   hljs_styles: { one: "one.css", two: "two.css", three: "three.css" },
 }
 
-describe("migrate_syntaxCSS", function () {
-  it("should not modify a valid css style", function () {
-    let options = { "syntax-css": "two.css" }
-    migrate_syntaxCSS(options, DEFAULTS)
-    expect(options["syntax-css"]).to.equal("two.css")
-  })
-  it("should reset an invalid css style to the default", function () {
-    let options = { "syntax-css": "BOGUS" }
-    migrate_syntaxCSS(options, DEFAULTS)
-    expect(options["syntax-css"]).to.be.undefined
-  })
-})
-
-describe("migrate_badMathValue", function () {
-  const MATH_FIXED = "some math data"
-  const J1_MATH = JSON.stringify(MATH_FIXED)
-  const J2_MATH = JSON.stringify(J1_MATH)
-  it("fix json encoded math value", function () {
-    let options = { "math-value": J1_MATH }
-    migrate_badMathValue(options)
-    expect(options["math-value"]).to.equal(MATH_FIXED)
-  })
-  it("fix 2x json encoded math value", function () {
-    let options = { "math-value": J2_MATH }
-    migrate_badMathValue(options)
-    expect(options["math-value"]).to.equal(MATH_FIXED)
-  })
-  it("ignore good math value", function () {
-    let options = { "math-value": MATH_FIXED }
-    migrate_badMathValue(options)
-    expect(options["math-value"]).to.equal(MATH_FIXED)
-  })
-})
-
-describe("migrate_MainCSS", function () {
-  it("update an old default css", async function () {
-    const EXPECTED = DEFAULTS["main-css"]
-    let options = { "main-css": "/* some css code */" }
-    await migrate_MainCSS(options, DEFAULTS)
-    expect(options["main-css"]).to.be.undefined
-  })
-  it("NOT update a custom css", async function () {
-    const CUSTOM_CSS = "/* custom css code */"
-    let options = { "main-css": CUSTOM_CSS }
-    await migrate_MainCSS(options, DEFAULTS)
-    expect(options["main-css"]).to.equal(CUSTOM_CSS)
-  })
-})
-
 describe("options_migrations tests", function () {
+  describe("migrate_syntaxCSS", function () {
+    it("should not modify a valid css style", async function () {
+      let options = { "syntax-css": "two.css" }
+      let changed = await migrate_syntaxCSS(options, DEFAULTS)
+      expect(changed).to.be.null
+    })
+    it("should reset an invalid css style to the default", async function () {
+      let options = { "syntax-css": "BOGUS" }
+      let changed = await migrate_syntaxCSS(options, DEFAULTS)
+      expect(changed["syntax-css"]).to.equal("three.css")
+    })
+  })
+
+  describe("migrate_badMathValue", function () {
+    const MATH_FIXED = "some math data"
+    const J1_MATH = JSON.stringify(MATH_FIXED)
+    const J2_MATH = JSON.stringify(J1_MATH)
+    it("fix json encoded math value", async function () {
+      let options = { "math-value": J1_MATH }
+      let changed = await migrate_badMathValue(options)
+      expect(changed["math-value"]).to.equal(MATH_FIXED)
+    })
+    it("fix 2x json encoded math value", async function () {
+      let options = { "math-value": J2_MATH }
+      let changed = await migrate_badMathValue(options)
+      expect(changed["math-value"]).to.equal(MATH_FIXED)
+    })
+    it("ignore good math value", async function () {
+      let options = { "math-value": MATH_FIXED }
+      let changed = await migrate_badMathValue(options)
+      expect(changed).to.be.null
+    })
+  })
+
+  describe("migrate_MainCSS", function () {
+    it("update an old default css", async function () {
+      const EXPECTED = DEFAULTS["main-css"]
+      let options = { "main-css": "/* some css code */" }
+      let changed = await migrate_MainCSS(options, DEFAULTS)
+      expect(changed["main-css"]).to.equal(EXPECTED)
+    })
+    it("NOT update a custom css", async function () {
+      const CUSTOM_CSS = "/* custom css code */"
+      let options = { "main-css": CUSTOM_CSS }
+      let changed = await migrate_MainCSS(options, DEFAULTS)
+      expect(changed).to.be.null
+    })
+  })
+
   describe("migrate_setLastVersion", function () {
     const thisVersion = messenger.runtime.getManifest().version
-    it("should update the last run version", function () {
+    it("should update the last run version", async function () {
       let options = { "last-version": "0.0.0" }
-      migrate_setLastVersion(options)
-      expect(options["last-version"]).to.equal(thisVersion)
+      let changed = await migrate_setLastVersion(options)
+      expect(changed["last-version"]).to.equal(thisVersion)
     })
 
-    it("should set the last run version", function () {
+    it("should set the last run version", async function () {
       let options = {}
-      migrate_setLastVersion(options)
-      expect(options["last-version"]).to.equal(thisVersion)
+      let changed = await migrate_setLastVersion(options)
+      expect(changed["last-version"]).to.equal(thisVersion)
     })
   })
 })
