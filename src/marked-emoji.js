3db88f076e3fb6dfcda405bccc37953d1dcba6cf
Rob Lemley <rob@thunderbird.net>
2023-02-26 14:19:16 -0500

    More eslint adjustments

--- src/marked-emoji.js
+++ src/marked-emoji.js
@@ -5,52 +5,58 @@
 
 const defaultOptions = {
   emojis: undefined, // required
-  unicode: false
+  unicode: false,
 }
 
 export function markedEmoji(options) {
   options = {
     ...defaultOptions,
-    ...options
+    ...options,
   }
 
   if (!options.emojis) {
-    throw new Error('Must provide emojis to markedEmoji')
+    throw new Error("Must provide emojis to markedEmoji")
   }
 
   return {
-    extensions: [{
-      name: 'emoji',
-      level: 'inline',
-      start(src) { return src.indexOf(':') },
-      tokenizer(src, tokens) {
-        const rule = /^:([\w+-]+?):/
-        const match = rule.exec(src)
-        if (!match) {
-          return
-        }
+    extensions: [
+      {
+        name: "emoji",
+        level: "inline",
+        start(src) {
+          return src.indexOf(":")
+        },
+        tokenizer(src, tokens) {
+          const rule = /^:([\w+-]+?):/
+          const match = rule.exec(src)
+          if (!match) {
+            return
+          }
 
-        const name = match[1]
-        const emoji = options.emojis[name]
+          const name = match[1]
+          const emoji = options.emojis[name]
 
-        if (!emoji) {
-          return
-        }
+          if (!emoji) {
+            return
+          }
 
-        return {
-          type: 'emoji',
-          raw: match[0],
-          name,
-          emoji
-        }
+          return {
+            type: "emoji",
+            raw: match[0],
+            name,
+            emoji,
+          }
+        },
+        renderer(token) {
+          if (options.unicode) {
+            return token.emoji
+          } else {
+            return `<img alt="${token.name}" src="${token.emoji}"${
+              this.parser.options.xhtml ? " /" : ""
+            }>`
+          }
+        },
       },
-      renderer(token) {
-        if (options.unicode) {
-          return token.emoji
-        } else {
-          return `<img alt="${token.name}" src="${token.emoji}"${this.parser.options.xhtml ? ' /' : ''}>`
-        }
-      }
-    }]
+    ],
   }
 }
