84f08d1551e106549f215702c99b3d9575246941
Rob Lemley <rob@calypsoblue.org>
2023-11-05 06:02:08 -0500

    Remove old test code

--- extension/test/test-run.js
+++ extension/test/test-run.js
@@ -1,12 +1,18 @@
 /*
+ * Copyright JFX 2021-2023
+ * MIT License
+ * https://gitlab.com/jfx2006
+ */
+
+/*
  * Copyright Adam Pritchard 2013
  * MIT License : http://adampritchard.mit-license.org/
  */
 
-$(function(){
+$(function () {
   mocha
     // I'm not sure what introduces the global "schemaTypes", but it's not
     // Markdown Here and it causes an error on one of my Chrome instances.
-    .globals([ 'schemaTypes' ]) // acceptable globals
-    .run();
-});
+    .globals(["schemaTypes"]) // acceptable globals
+    .run()
+})
