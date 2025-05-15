a9452a9645f5ed482e8147dd08256f8e53043038
Rob Lemley <rob@calypsoblue.org>
2025-05-14 23:43:34 -0400

    Refactor css-inliner to be less insane.
    - No need to precompute default styles. Just cache when first calculated.
    - Use `document.defaultView.getDefaultComputedStyle` instead of a dummy fake document.
    - Use `document.defaultView.getComputedStyle` instead of `window.getComputedStyle`.
    - Only check allow-listed CSS properties. Many properties don't work in email or don't make sense.

--- extension/compose_preview/css-inliner.js
+++ extension/compose_preview/css-inliner.js
@@ -1,167 +1,65 @@
 /*
- * Copyright JFX 2021-2023
+ * Copyright JFX 2021-2025
  * MIT License
  * https://gitlab.com/jfx2006
  */
 
-function computeDefaultStyleByTagName(msgDocument, tagName) {
-  const defaultElemStyle = {}
-  const element = msgDocument.createElement(tagName)
 
-  const computedStyleAttached = getComputedStyle(msgDocument.body.appendChild(element))
-  const display = computedStyleAttached.getPropertyValue("display")
-  element.style.display = "none"
-  for (let i = 0; i < computedStyleAttached.length; i++) {
-    defaultElemStyle[computedStyleAttached[i]] = computedStyleAttached[computedStyleAttached[i]]
-  }
-
-  defaultElemStyle["display"] = display
-
-  msgDocument.body.removeChild(element)
-  return defaultElemStyle
-}
-
-const precomputeTags = [
-  "A",
-  "ABBR",
-  "ADDRESS",
-  "AREA",
-  "ARTICLE",
-  "ASIDE",
-  "AUDIO",
-  "B",
-  "BASE",
-  "BDI",
-  "BDO",
-  "BLOCKQUOTE",
-  "BODY",
-  "BR",
-  "BUTTON",
-  "CANVAS",
-  "CAPTION",
-  "CENTER",
-  "CITE",
-  "CODE",
-  "COL",
-  "COLGROUP",
-  "COMMAND",
-  "DATALIST",
-  "DD",
-  "DEL",
-  "DETAILS",
-  "DFN",
-  "DIV",
-  "DL",
-  "DT",
-  "EM",
-  "EMBED",
-  "FIELDSET",
-  "FIGCAPTION",
-  "FIGURE",
-  "FONT",
-  "FOOTER",
-  "FORM",
-  "H1",
-  "H2",
-  "H3",
-  "H4",
-  "H5",
-  "H6",
-  "HEAD",
-  "HEADER",
-  "HGROUP",
-  "HR",
-  "HTML",
-  "I",
-  "IFRAME",
-  "IMG",
-  "INPUT",
-  "INS",
-  "KBD",
-  "KEYGEN",
-  "LABEL",
-  "LEGEND",
-  "LI",
-  "LINK",
-  "MAP",
-  "MARK",
-  "MATH",
-  "MENU",
-  "META",
-  "METER",
-  "NAV",
-  "NOBR",
-  "NOSCRIPT",
-  "OBJECT",
-  "OL",
-  "OPTION",
-  "OPTGROUP",
-  "OUTPUT",
-  "P",
-  "PARAM",
-  "PRE",
-  "PROGRESS",
-  "Q",
-  "RP",
-  "RT",
-  "RUBY",
-  "S",
-  "SAMP",
-  "SCRIPT",
-  "SECTION",
-  "SELECT",
-  "SMALL",
-  "SOURCE",
-  "SPAN",
-  "STRONG",
-  "STYLE",
-  "SUB",
-  "SUMMARY",
-  "SUP",
-  "SVG",
-  "TABLE",
-  "TBODY",
-  "TD",
-  "TEXTAREA",
-  "TFOOT",
-  "TH",
-  "THEAD",
-  "TIME",
-  "TITLE",
-  "TR",
-  "TRACK",
-  "U",
-  "UL",
-  "VAR",
-  "VIDEO",
-  "WBR",
+const ALLOW_CSS_PROPS = [
+  /^--.*$"/i,
+  /^align.*$/i,
+  /^background.*$/i,
+  /^border.*$/i,
+  /^bottom$/i,
+  /^color.*$/i,
+  /^column.*$/i,
+  /^contain$/i,
+  /^content.*$/i,
+  /^display$/i,
+  /^empty-cells$/i,
+  /^flex.*$/i,
+  /^float$/i,
+  /^font-.*$/i,
+  /^gap$/i,
+  /^grid.*$/i,
+  /^height$/i,
+  /^hyphen.*$/i,
+  /^inline-size$/i,
+  /^inset.*$/i,
+  /^isolation$/i,
+  /^letter-spacing$/i,
+  /^line-.*$/i,
+  /^line-height$/i,
+  /^list-style.*$/i,
+  /^margin.*$/i,
+  /^max-.*$/i,
+  /^min-.*$/i,
+  /^object-.*$/i,
+  /^opacity$/i,
+  /^order$/i,
+  /^outline.*$/i,
+  /^overflow.*$/i,
+  /^padding.*$/i,
+  /^position$/i,
+  /^quotes$/i,
+  /^tab-size$/i,
+  /^table-layout$/i,
+  /^text-.*$/i,
+  /^top$/i,
+  /^unicode-bidi$/i,
+  /^vertical-align$/i,
+  /^visibility$/i,
+  /^white-space.*$/i,
+  /^width$/i,
+  /^word-.*$/i,
 ]
 
-function mkBlankDocument() {
-  const parser = new DOMParser()
-  return parser.parseFromString(
-    '<!doctype html>\n<html lang="en"><head><title></title></head><body></body></html>',
-    "text/html",
-  )
-}
 
-function computeDefaults(msgDocument) {
-  const defaults = {}
-  if (msgDocument.body === null) {
-    throw "msgDocument.body must exist before creating CSSInliner"
-  }
-  for (let i = 0; i < precomputeTags.length; i++) {
-    defaults[precomputeTags[i]] = computeDefaultStyleByTagName(msgDocument, precomputeTags[i])
-  }
-  return defaults
-}
 
 export class CSSInliner {
   #defaultStyles
-  #blankDocument
   constructor() {
-    this.#blankDocument = mkBlankDocument()
-    this.#defaultStyles = computeDefaults(this.#blankDocument)
+    this.#defaultStyles = {}
   }
   // inlineStylesForSingleElement(element, target): inlines the styles for a
   // single element, but not it's children
@@ -169,33 +67,19 @@ export class CSSInliner {
   // Params:
   // element: The element that computed styles inlined
   inlineStylesForSingleElement(element) {
-    const computedStyle = getComputedStyle(element)
-    const display = computedStyle.getPropertyValue("display")
-    element.style.display = "none"
+    const computedStyle = document.defaultView.getComputedStyle(element)
     if (this.#defaultStyles[element.tagName] == null) {
-      this.#defaultStyles[element.tagName] = computeDefaultStyleByTagName(
-        this.#blankDocument,
-        element.tagName,
-      )
+      this.#defaultStyles[element.tagName] = document.defaultView.getDefaultComputedStyle(element)
     }
     for (let i = 0; i < computedStyle.length; i++) {
       const styleName = computedStyle[i]
-      if (styleName.startsWith("-webkit") || styleName === "cursor") {
-        continue
-      }
-
-      // exclude default styles
-      if (this.#defaultStyles[element.tagName][styleName] !== computedStyle[styleName]) {
-        element.style[styleName] = computedStyle[styleName]
-      }
-
-      if (styleName === "display" && this.#defaultStyles[element.tagName]["display"] !== display) {
-        element.style["display"] = display
-      } else if (styleName === "display") {
-        element.style["display"] = ""
+      if (ALLOW_CSS_PROPS.some((regex) => styleName.match(regex)?.length > 0)) {
+        // exclude default styles
+        if (this.#defaultStyles[element.tagName][styleName] !== computedStyle[styleName]) {
+          element.style[styleName] = computedStyle[styleName]
+        }
       }
     }
-
     if (element.style.length === 0) {
       element.removeAttribute("style")
     }
