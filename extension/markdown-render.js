56859b3e3b32acb9c00f40912e300050325a5792
Rob Lemley <rob@calypsoblue.org>
2025-10-16 04:59:16 -0400

    Revert "Disable inline directive syntax. #138"
    This reverts commit d5a251b76834482a8d3076dcdd3d815de77ab0da.

--- extension/markdown-render.js
+++ extension/markdown-render.js
@@ -19,7 +19,7 @@ import hljs from "./highlightjs/highlightjs.esm.js"
 import { markedHighlight } from "./vendor/marked-highlight.esm.js"
 import markedExtendedTables from "./vendor/marked-extended-tables.esm.js"
 import markedLinkifyIt from "./vendor/marked-linkify-it.esm.js"
-import { createDirectives } from "./vendor/marked-directive.esm.js"
+import { createDirectives, presetDirectiveConfigs } from "./vendor/marked-directive.esm.js"
 import { urlSchemify } from "./marked-link-scheme.esm.js"
 
 import OptionsStore from "./options/options-storage.js"
@@ -30,12 +30,6 @@ const defaultMarkedOptions = Object.assign({}, marked.getDefaults(), {
   headerPrefix: undefined,
   smartypants: undefined,
 })
-
-const directiveConfig = [
-  { level: "container", marker: ":::" },
-  { level: "block", marker: "::" },
-  // { level: "inline", marker: ":" }
-]
 
 export async function resetMarked(userprefs) {
   marked.setOptions(defaultMarkedOptions)
@@ -82,7 +76,7 @@ export async function resetMarked(userprefs) {
     }
     marked.use(markedMath(mathOptions))
   }
-  marked.use(createDirectives([...directiveConfig]))
+  marked.use(createDirectives([...presetDirectiveConfigs]))
   if (userprefs["emoji-shortcode-enabled"]) {
     const { markedEmoji } = await import("./vendor/marked-emoji.esm.js")
     const { default: emojis } = await import("./data/shortcodes.mjs")
