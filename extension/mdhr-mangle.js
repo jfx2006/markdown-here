72b15c59aa861ec83c719c8385b6b0c647422fd5
Rob Lemley <rob@calypsoblue.org>
2025-12-13 10:18:08 -0500

    Improve markdown rendering in paragraph mode.
    Convert `<p>stuff</p>` to `stuff<br>\n`.

--- extension/mdhr-mangle.js
+++ extension/mdhr-mangle.js
@@ -29,10 +29,11 @@ export class MdhrMangle {
 
   async preprocess() {
     await this.excludeContent()
+    this.convertParagraphs()
     this.insertLinebreaks()
     this.convertHTML()
     const text = await convertToText(this.doc.body)
-    return text.replaceAll(" ", " ")
+    return text.replaceAll(" ", " ").replaceAll("\n\n", "\n")
   }
 
   async excludeContent() {
@@ -42,7 +43,6 @@ export class MdhrMangle {
     }
 
     const excluded = this.doc.querySelectorAll(
-      // eslint-disable-next-line max-len
       "body > blockquote[type='cite'], body > .moz-signature, body > div.moz-forward-container, img, div.mdhr-raw",
     )
     for (const e of excluded) {
@@ -53,6 +53,19 @@ export class MdhrMangle {
       placeholderElem.innerText = placeholder
       e.replaceWith(placeholderElem)
     }
+  }
+
+  convertParagraphs() {
+    const paragraphs = this.doc.querySelectorAll("body > p")
+    paragraphs.forEach((p, index) => {
+      const frag = this.doc.createDocumentFragment()
+      frag.replaceChildren(...p.childNodes)
+      const isLast = index === paragraphs.length - 1
+      if (!isLast) frag.appendChild(this.doc.createElement("br"))
+      frag.appendChild(this.doc.createTextNode("\n"))
+
+      p.replaceWith(frag)
+    })
   }
 
   insertLinebreaks() {
